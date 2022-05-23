aef7e75a9fd1e1be6a6b04902e5bff169042eab3
EDR3X <dhunganaannanta@gmail.com>
2022-05-23 22:14:21 +0545

    crash fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -9,7 +9,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await dotenv.load(fileName: ".env");
 
   runApp(const MyApp());
 }
